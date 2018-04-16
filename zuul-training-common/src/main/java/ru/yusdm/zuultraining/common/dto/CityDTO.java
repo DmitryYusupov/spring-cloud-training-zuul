e8c1fc1132ac1eb0e45d8c283b1c4b083d0d3607
Dmitry <us@tt.ru>
2018-04-16 22:47:45 +0400

    Initial commit, added dtos

---
+++ zuul-training-common/src/main/java/ru/yusdm/zuultraining/common/dto/CityDTO.java
@@ -0,0 +1,52 @@
+package ru.yusdm.zuultraining.common.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class CityDTO implements Serializable {
+    private Long id;
+    private Long countryId;
+    private String name;
+    private int population;
+    private List<StreetDTO> streets;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public List<StreetDTO> getStreets() {
+        return streets;
+    }
+
+    public void setStreets(List<StreetDTO> streets) {
+        this.streets = streets;
+    }
+
+    public Long getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(Long countryId) {
+        this.countryId = countryId;
+    }
+}
---
+++ zuul-training-common/src/main/java/ru/yusdm/zuultraining/common/dto/StreetDTO.java
@@ -0,0 +1,33 @@
+package ru.yusdm.zuultraining.common.dto;
+
+import java.io.Serializable;
+
+public class StreetDTO implements Serializable{
+    private Long id;
+    private Long cityId;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(Long cityId) {
+        this.cityId = cityId;
+    }
+}
