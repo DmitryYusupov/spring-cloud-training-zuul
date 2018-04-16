6d394797ac083fbfdfc49767a09893e360a00bd2
Dmitry <us@tt.ru>
2018-04-17 00:19:16 +0400

    added extra modules

---
+++ street-service/src/main/java/ru/yusdm/cloudtraining/zuul/streetservice/StreetServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.yusdm.cloudtraining.zuul.streetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetServiceApplication.class, args);
+	}
+}
---
+++ street-service/src/test/java/ru/yusdm/cloudtraining/zuul/streetservice/StreetServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.yusdm.cloudtraining.zuul.streetservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StreetServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
